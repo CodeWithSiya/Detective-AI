9380ce4d759c938d63fc0613da46c8478b1792c3
Ethan Ngwetjana <ethanngwetjana@gmail.com>
2025-08-17 10:10:43 +0200

    AICD - Defined basic behaviour of the login form

--- frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import './LoginSignup.css'
+import React, { useState } from 'react';
+import './LoginSignup.css';
 
 export const LoginSignup = () => {
-  return (
-    
-    <section class="container">
-      <form id="my-form">
-        <h1>Welcome back</h1>
-        <h2>Sign in to continue</h2>
-        <div class="msg"></div>
-        <div>
-          <label for="email">Email:</label>
-          <input type="text" id="email"></input>
-        </div>
-        <div>
-          <label for="password">Password:</label>
-          <input type="text" id="email"></input>
-        </div>
-        <input class="btn" type="submit" value="Sign In"></input>
-      </form>
 
-    </section>
-  )
-}
+    //Update state as user inputs
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleSubmit = (e) => {
+        //Prevent default submit behaviour
+        e.preventDefault();
+
+        //Log values to console
+        console.log("Email:", email);
+        console.log("Password:", password);
+    };
+
+    return (
+        <section className="container">
+        <form id="my-form" onSubmit={handleSubmit}>
+            <h1>Welcome back</h1>
+            <h2>Sign in to continue</h2>
+            <div className="msg"></div>
+
+            <div>
+            <label>Email:</label>
+            <input 
+                type="email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            </div>
+
+            <div>
+            <label>Password:</label>
+            <input 
+                type="password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            </div>
+
+            <input className="btn" type="submit" value="Login" />
+        </form>
+        </section>
+    );
+};
