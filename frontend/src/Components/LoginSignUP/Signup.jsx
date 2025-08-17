c593d1e6be0b7fbf711c83de4fc93ee8019a1ad3
Ethan Ngwetjana <ethanngwetjana@gmail.com>
2025-08-18 00:06:47 +0200

    AICD - Added the rest of the components for the sign up page

--- frontend/src/Components/LoginSignUP/Signup.jsx
+++ frontend/src/Components/LoginSignUP/Signup.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FormControl, FormLabel } from '@chakra-ui/form-control';
 import { motion } from 'framer-motion'
 import { Typewriter } from 'react-simple-typewriter';
+import { Link as RouterLink } from "react-router-dom";
 
 import {
   Box,
@@ -15,11 +16,104 @@ import {
   VStack,
   Checkbox,
   Stack,
+  HStack
 } from "@chakra-ui/react";
 
 
+
 const Signup = () => {
-    return <h1>Hello World</h1>;
+
+    const MotionBox = motion(Box);
+
+    return (
+        //Container for background
+        <Flex
+            minH={'100vh'} //take full height
+            align={'center'} //vertically center
+            justify={'center'} //horizontal center
+            bgGradient={"linear(to-r, purple.600, purple.400)"} //Purple Gradient
+        >
+            <Stack 
+                spacing={8} //space between stacked children
+                mx={'auto'} //Centre stack horizontally
+                maxW={'lg'} //Max width
+                py={12} // p-top and p-bottonm
+                px={6} // p-left and p-right
+            >
+
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}>
+                        Join the Squad! 🕵🏽
+                    </Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        Create an account to continue
+                    </Text>
+                </Stack>
+
+                <MotionBox
+                    rounded={'lg'} //rounded corners
+                    bg={"white"}
+                    boxShadow={'lg'} //Shadow-effect behind card
+                    p={8} //padding
+                    w="500px"
+
+                    //Animate box
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5, ease: 'easeOut' }}
+                >
+                    <Stack spacing={4}>
+                    
+                        <HStack>
+                            <Box flex={1}>
+                                <FormControl id="firstName" isRequired>
+                                    <FormLabel>First Name</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box>
+                            <Box flex={1}> 
+                                <FormControl id="lastName">
+                                    <FormLabel>Last Name</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box>
+                        </HStack>
+
+                        <FormControl id="email" isRequired>
+                            <FormLabel>Email address</FormLabel>
+                            <Input type="email" />
+                        </FormControl>
+
+                        <FormControl id="password" isRequired>
+                            <FormLabel>Password</FormLabel>
+                            <Input type="password" />
+                        </FormControl>
+
+                        <Button
+                            bg={'purple.400'}
+                            variant={'subtle'}
+                            color={'white'}
+                            _hover={{
+                            bg: 'purple.500',
+                            }}
+                        >
+                            Sign up
+                        </Button>
+
+                        <Text textAlign="center">
+                                Already a user?{" "}
+                            <Link as={RouterLink} to="/" color="purple.600" >
+                                Log in
+                            </Link>
+                        </Text>
+
+                    </Stack>
+                    
+                </MotionBox>
+            </Stack>
+
+        </Flex>
+    )
 }
 
 export default Signup
