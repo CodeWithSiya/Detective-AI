27a8ab2261fd552bf1149048cd893a90c6258088
Ethan Ngwetjana <ethanngwetjana@gmail.com>
2025-08-17 12:26:13 +0200

    AICD - Added more components to the login page (Remember me, Forgot password, etc.)

--- frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -6,6 +6,8 @@ export const LoginSignup = () => {
     //Update state as user inputs
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState('');
+
 
 
     const handleSubmit = (e) => {
@@ -17,13 +19,36 @@ export const LoginSignup = () => {
         console.log("Password:", password);
     };
 
+    const handleForgotPassword = () => {
+
+    }
+
+    const handlesSignup = () => {
+
+    }
+
+    const handleGoogleLogin = () => {
+
+    }
+
+    const handleGithubLogin = () => {
+
+    }
+
+    const handleFacebookLogin = () => {
+        
+    }
+
     return (
         <section className="container">
         <form id="my-form" onSubmit={handleSubmit}>
+
+            {/* Header */}
             <h1>Welcome back</h1>
             <h2>Sign in to continue</h2>
             <div className="msg"></div>
 
+            {/* Email field */}
             <div>
             <label>Email:</label>
             <input 
@@ -33,6 +58,7 @@ export const LoginSignup = () => {
             />
             </div>
 
+            {/* Password field*/}
             <div>
             <label>Password:</label>
             <input 
@@ -42,7 +68,49 @@ export const LoginSignup = () => {
             />
             </div>
 
-            <input className="btn" type="submit" value="Login" />
+            {/* Remember me checkbox and forgot password*/}
+            <div>
+                <label>
+                    <input 
+                        type="checkbox" 
+                        checked={rememberMe}
+                        onChange={(e) => setRememberMe(e.target.checked)}
+                    />
+                    Remember me
+                </label>
+
+
+                <button type='button' onClick={handleForgotPassword}>
+                    Forgot password?
+                </button>
+            </div>
+
+            
+
+            <button type='button'>Login</button>
+
+            {/* Divider */}
+            <div>
+                <span>or sign in with</span>
+            </div>
+
+            {/* Third-party sign in buttons*/}
+            <div>
+                <button type="button" onClick={handleGoogleLogin}>Google</button>
+                <button type="button" onClick={handleGithubLogin}>GitHub</button>
+                <button type="button" onClick={handleFacebookLogin}>Facebook</button>
+            </div>
+
+            {/* Sign up link */}
+            <div>
+                <p>
+                    Don't have an account
+                    <button type="button" onClick={handlesSignup}>
+                        Sign up
+                    </button>    
+                </p>
+            </div>
+
         </form>
         </section>
     );
