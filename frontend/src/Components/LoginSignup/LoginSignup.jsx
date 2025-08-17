c3179de78795d6ddba830714e0379d55b3d45895
Ethan Ngwetjana <ethanngwetjana@gmail.com>
2025-08-17 22:33:51 +0200

    AICD - Added the rest of the components to the login card, widened the card

--- frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -12,7 +12,10 @@ import {
   Text,
   Link,
   VStack,
+  Checkbox,
+  Stack,
 } from "@chakra-ui/react";
+import { LuTypeOutline } from 'react-icons/lu';
 
 export const LoginSignup = () => {
 
@@ -54,39 +57,84 @@ export const LoginSignup = () => {
             justify={'center'} //horizontal center
             bgGradient={"linear(to-r, purple.600, purple.400)"} //Purple Gradient
         >
-            <VStack 
+            <Stack 
                 spacing={8} //space between stacked children
                 mx={'auto'} //Centre stack horizontally
                 maxW={'lg'} //Max width
                 py={12} // p-top and p-bottonm
                 px={6} // p-left and p-right
-            ></VStack>
-
-            <VStack align={'center'}>
-                <Heading fontSize={'4x1'}>Welcome back ☺️</Heading>
-                <Text fontSize={'lg'} color={'gray.600'}>
-                    Log in to your account to continue
-                </Text>
-            </VStack>
-
-            {/*Card */}
-            <Box
-                rounded={'lg'} //rounded corners
-                bg={"white"}
-                boxShadow={'lg'} //Shadow-effect behind card
-                p={8} //padding
             >
-                <VStack spacing={4}>
 
-                    <FormControl id="email">
-                        <FormLabel>Email address</FormLabel>
-                        <Input type="email" />
-                    </FormControl>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}>Welcome back ☺️</Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        Log in to your account to continue
+                    </Text>
+                </Stack>
 
-                </VStack>
-                
-            </Box>
+                {/*Card */}
+                <Box
+                    rounded={'lg'} //rounded corners
+                    bg={"white"}
+                    boxShadow={'lg'} //Shadow-effect behind card
+                    p={8} //padding
+                    w="500px"
+                >
+                    <Stack spacing={4}>
+                    
+                        <FormControl id="email">
+                            <FormLabel>Email address</FormLabel>
+                            <Input type="email" />
+                        </FormControl>
 
+                        <FormControl id="password">
+                            <FormLabel>Password</FormLabel>
+                            <Input type="password" />
+                        </FormControl>
+
+                        <Stack
+                            direction={{ base: 'column', sm: 'row' }}
+                            align={'start'}
+                            justify={'space-between'}
+                        >
+                            {/*Checkbox*/}
+                            <Checkbox.Root
+                                variant={'subtle'}
+                                colorPalette={'purple'}
+                            >
+                                <Checkbox.HiddenInput />
+                                <Checkbox.Control />
+                                <Checkbox.Label>Remember me</Checkbox.Label>
+                            </Checkbox.Root>
+                            
+                            <Link color="purple.500" fontSize="sm">
+                                Forgot password?
+                            </Link>
+
+                        </Stack>
+
+                        <Button
+                            bg={'purple.400'}
+                            variant={'subtle'}
+                            color={'white'}
+                            _hover={{
+                            bg: 'purple.500',
+                            }}
+                        >
+                            Log in
+                        </Button>
+
+                        <Text textAlign="center">
+                                Don’t have an account?{" "}
+                            <Link color="purple.600" >
+                                Sign up
+                            </Link>
+                        </Text>
+
+                    </Stack>
+                    
+                </Box>
+            </Stack>
 
 
 
