a158d8e01f4470c0b39cd87279364153465a3e54
Ethan Ngwetjana <ethanngwetjana@gmail.com>
2025-08-20 20:57:18 +0200

    AICD - Changed App to be consistennt with other branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './Components/UserAuthentication/Login'
-import Signup from './Components/UserAuthentication/Signup';
-import ForgotPassword from './Components/UserAuthentication/ForgotPassword';
-import VerifyEmail from './Components/UserAuthentication/VerifyEmail';
-import ChangePassword from './Components/UserAuthentication/ChangePassword'
+import Login from './Components/LoginSignUP/Login'
+import Signup from './Components/LoginSignUP/Signup';
+import ForgotPassword from './Components/LoginSignUP/ForgotPassword';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DetectivePage from './Components/DetectivePage/DetectivePage';
+import DemoLandingPage from './Components/DemoLandingPage/DemoLandingPage';
+import Team from './Components/Team/Team';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<DemoLandingPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/verify-email" element={<VerifyEmail />}/>
-        <Route path="/change-password" element={<ChangePassword/>}/>
+        <Route path="/detective" element={<DetectivePage />} />
+        <Route path="/team" element={<Team />} />
       </Routes>
     </Router>
   )
 }
-
 export default App
