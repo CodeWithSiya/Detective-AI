53c85b9167cfb4b00fda8a2c9a786dd88828505e
Siyabonga Madondo <MDNSIY014@myuct.ac.za>
2025-08-21 21:12:12 +0200

    AICD - Initialised AI analysis services for business logic.

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -2,4 +2,9 @@
 Services package initialisation.
 """
 # Initialising text extraction service.
-from .TextExtractor import TextExtractor
+from .text_extractor import TextExtractor
+
+# Initialising analysis extraction services.
+from .ai_analyser import AiAnalyser
+from .ai_text_analyser import AiTextAnalyser
+from .ai_image_analyser import AiImageAnalyser
