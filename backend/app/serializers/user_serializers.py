931a3c0fbf2afda76cc31de3db44e0ffae9db2ca
Siyabonga Madondo <MDNSIY014@myuct.ac.za>
2025-09-08 11:50:45 +0200

    AICD - Updated user serialisers.

---
+++ backend/app/serializers/user_serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for User model.
+
+    :author: Siyabonga Madondo, Ethan Ngwetjana, Lindokuhle Mdlalose
+    :version: 22/08/2025
+    """
+    is_admin = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'user_type', 'is_admin']
+
+    def get_is_admin(self, obj):
+        return obj.is_admin_user()
