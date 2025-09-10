b5ebf7e24428fe140e053dbc1915ca0977a9b708
Lindokuhle239 <mdlaloselyndorh@gmail.com>
2025-09-10 15:53:55 +0200

    style: Remove MotionBox and refactor ForgotPassword component layout

--- frontend/src/Components/UserAuthentication/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/Components/UserAuthentication/ForgotPassword/ForgotPassword.jsx
@@ -38,10 +38,6 @@ export const getEmail = () => {
  * @returns {JSX.Element} ForgotPasswordComponent
  */
 const ForgotPassword = () => {
-
-    //Initialise motion box for animation
-    const MotionBox = motion(Box);
-
     //Initialise navigator for navigating between routes
     const navigate = useNavigate();
 
@@ -69,106 +65,12 @@ const ForgotPassword = () => {
             //Error message if not found/invalid
             alert(result.message);
         }
-        
-
-
     }
 
     return (
-
-        //Main container
-        <Flex
-            minH={'100vh'} //take full viewport height
-            align={'center'} //vertically center
-            justify={'center'} //horizontal center
-        >
-            {/* Main content stack*/}
-            <Stack 
-                align={'center'} //Center children horizontally
-                spacing={4} //space between stacked children
-                mx={'auto'} //Centre stack horizontally
-                maxW={'lg'} //Max width
-                py={10} // p-top and p-bottonm
-                px={6} // p-left and p-right
-            >
-                {/* Log and header section */}
-                <Stack align={'center'}>
-
-                    {/*Logo*/}
-                    <Image src="/src/Components/Assets/Logo.jpg" alt="Logo" boxSize="250px" mb={4} />
-
-                    {/* Heading with typewriter effect */}
-                    <Heading fontSize={'4xl'} color={"black"}>
-                        <Typewriter
-                            words={['Forgot your Password?',]} //what will be typed
-                            loop={1}//amount of times animation plays
-                            cursor //Blinking cursor
-                            cursorStyle="_"
-                            typeSpeed={70} //typing speed in ms
-                            deleteSpeed={50} //deletion speed in ms
-                            delaySpeed={1000} //Delay before starting in ms
-                        />
-                    </Heading>
-
-                    {/*Subtitle*/}
-                    <Text fontSize={'lg'} color={'gray.600'}>
-                        You&apos;ll get an email with a reset link
-                    </Text>
-                </Stack>
-               
-                {/* Main form with animation */}
-                <MotionBox
-                w='500px' //fixed width
-                p={6}      //internal padding
-                my={12} //Vertical margin
-                rounded={'lg'} //large rounded corners
-                bg={"white"}  //White background
-                boxShadow={'lg'} //large shadow drop
-
-                //Animate box
-                initial={{ opacity: 0, scale: 0.8 }} //Start small and invisible
-                animate={{ opacity: 1, scale: 1 }} //Animate to visible and full size
-                transition={{ duration: 0.5, ease: 'easeOut' }} //Smooth transition
-            >
-                {/* Form container */}
-                <Stack spacing={4}>
-
-                {/* Email input field */}
-                <FormControl id="email">
-                    <Field.Root>
-                        <Field.Label>Email</Field.Label>
-                        <Input placeholder="johndoe@example.com" ref={emailRef}/>
-                    </Field.Root>
-                </FormControl>
-
-                {/* Submit button section */}
-                <Stack spacing={6} pt={6}>
-
-                    <Button
-                        bg={'black'} //black background
-                        color={'white'}  //white text
-                        _hover={{   //Hover state styling
-                        bg: 'blackAlpha.800',}}
-                        onClick={handleSubmit}
-                    >
-                        Request Reset
-                    </Button>
-                </Stack>
-
-            </Stack>
-            </MotionBox>
-
-            </Stack>
-
         
-
-            
-
-
-        </Flex>
-    )
-
-}
+    );
+};
 export default ForgotPassword;
 
 
