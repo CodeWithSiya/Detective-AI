a71d4834e0ef734dd27ceda060071ce92353a62f
Siyabonga Madondo <MDNSIY014@myuct.ac.za>
2025-09-20 00:13:06 +0200

    fix: Correct route path for DetectiveBasic component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/UserAuthentication/Login/Login'
 import Signup from './components/UserAuthentication/Signup/Signup';
@@ -12,8 +9,8 @@ import DetectivePage from './Components/DetectivePage/DetectivePage';
 import DemoLandingPage from './Components/DemoLandingPage/DemoLandingPage';
 import Team from './Components/Team/Team';
 import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
-import BasicDetectivePage from './Components/Basic-DetectivePage/BasicDetectivePage';
 import Admin from './Components/Admin/Admin';
+import DetectiveBasic from './components/Basic-DetectivePage/DetectiveBasic'
 
 function App() {
   return (
@@ -28,7 +25,7 @@ function App() {
         <Route path="/change-password" element={<ChangePassword/>}/>
         <Route path="/detective" element={<DetectivePage />} />
         <Route path="/team" element={<Team />} />
-        <Route path="/basic-detective" element={<BasicDetectivePage />} />
+        <Route path="/detective-basic" element={<DetectiveBasic />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
